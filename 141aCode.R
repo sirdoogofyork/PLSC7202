0e530cf7d5101212ea63f57d0207284f76959ff7
sirdoogofyork <jakemarkholley@gmail.com>
2020-10-02 09:57:47 -0400

    Added Replicate to Stats

--- 141aCode.R
+++ 141aCode.R
@@ -25,21 +25,21 @@ n3<-length(DLI$Harvest)
 n4<-length(suphrs$Date)
 n5<-length(NightAvg$Treatment)
 
-#Add Data from Supplemental Light Maps 
+#Add Data from Supplemental Light Maps
 for (x in 1:n1){
   for (a in 1:n2){
     if (plantdata$Replicate[x] == light$Replicate[a] & plantdata$Treatment[x] == light$Treatment[a] & plantdata$Raft[x] == light$Raft[a]& plantdata$Number[x] == light$Number[a]){
       plantdata$PAR[x] <- light$PAR[a] }
-    
+
   }
 }
 
-#Add DLI Data 
+#Add DLI Data
 for (x in 1:n1){
   for (a in 1:n3){
     if (plantdata$Date[x] == DLI$Harvest[a] & plantdata$Treatment[x] == DLI$Treatment[a]){
       plantdata$DLI[x] <- DLI$DLI[a] }
-    
+
   }
 }
 
@@ -48,7 +48,7 @@ for (x in 1:n1){
   for (a in 1:n4){
     if (plantdata$Date[x] == suphrs$Date[a]){
       plantdata$Hrsfix[x] <- suphrs$Hours[a] }
-    
+
   }
 }
 
@@ -57,7 +57,7 @@ for (x in 1:n1){
   for (a in 1:n4){
     if (plantdata$Treatment[x] == NightAvg$Treatment[a] & plantdata$Replicate[x] == NightAvg$Replicate[a]){
       plantdata$nightavg[x] <- NightAvg$NightAverage[a] }
-    
+
   }
 }
 
@@ -65,11 +65,9 @@ plantdata$DLIsun<-plantdata$DLI-(plantdata$nightavg*plantdata$Hrsfix*3600/100000
 plantdata$DLIfix<-plantdata$PAR*3600*plantdata$Hrsfix/1000000
 plantdata$trueDLI<-plantdata$DLIfix+plantdata$DLIsun
 
-md.1 <- lmer(FreshWeight ~ Treatment + Raft + Number + Variety + trueDLI + Spectrum + (1|Treatment)
+md.1 <- lmer(FreshWeight ~ Treatment + Raft + Number + Variety + trueDLI + Spectrum + Replicate + (1|Treatment)
              + (1|Treatment:Raft) + (1|Treatment:Raft:Number), data=plantdata)
 summary(md.1)
 anova(md.1)
 
 emmeans(md.1, pairwise ~ Spectrum|Variety)
-
-
